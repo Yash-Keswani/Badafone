fcbb051c213872e6442851c312255b8d9ac99566
Yash Keswani <yash20158@iiitd.ac.in>
2022-04-19 17:56:58 +0530

    trying that again lol

--- badafone/badafone/settings.py
+++ badafone/badafone/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
+STATIC_ROOT = 'staticfiles/'
 STATICFILES_DIRS = [
 	BASE_DIR / "static",
 	]
