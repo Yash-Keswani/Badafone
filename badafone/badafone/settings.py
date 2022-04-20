bb83f9ebc8314d2ebe15ec921224cc789021cff1
Yash Keswani <yash20158@iiitd.ac.in>
2022-04-20 20:52:34 +0530

    work? pretty please?

--- badafone/badafone/settings.py
+++ badafone/badafone/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATICFILES_STORAGE = 'my_project.storage.WhiteNoiseStaticFilesStorage'
+STATICFILES_STORAGE = 'badafone.storage.WhiteNoiseStaticFilesStorage'
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
